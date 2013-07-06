c6a9a56f3616aac98dda182ae216d3383e0099c9
igiga <ashahi@yahoo.com>
2013-07-06 05:21:26 +0000

    added hello world from index.html

--- web.js
+++ web.js
@@ -6,10 +6,11 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   var buf = new Buffer(256);
-  response.send(buf.toString('utf8', fs.readFileSync("index.html")))});
+  buf = fs.readFileSync("index.html");
+  response.send(buf.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
